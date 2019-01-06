09f4b57b44d6e3e84d6bd986d5166b23c7ed4a02
Liangchen <leeonky@gmail.com>
2019-01-06 14:33:20 +0800

    Init project with failed test

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        fail("test failed");
+    }
+}
+
